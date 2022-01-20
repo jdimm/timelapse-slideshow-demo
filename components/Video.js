a3af94b0228ed3c297cd0f8c0c39ed1d4c959169
John Dimm <jdimm@yahoo.com>
2022-01-19 18:27:29 -0800

    video controls loop autoPlay muted

--- components/Video.js
+++ components/Video.js
@@ -5,7 +5,7 @@ const Video = ({serial, camera} ) => {
     const src = `https://gardynappprod.blob.core.windows.net/timelapse-staging/videos/${serial}/camera0${camera}.mp4`
     return (
         <div className={styles.video_div}>
-            <video controls autoPlay>
+            <video controls loop autoPlay muted>
             <source src={src} type="video/mp4"/>
                 Sorry, your browser does not support embedded videos.
             </video>
