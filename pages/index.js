1bcb665c1ed6d3aa9c9b98b47b4fb9b7ceb5d473
jdimm <92388963+jdimm@users.noreply.github.com>
2022-05-14 10:07:08 -0700

    List of timelapse slideshows for list of serials, the nice ones.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import Slideshow from '../components/Slideshow'
+import nice from '../data/nice'
 
 function Index() {
-  const serial = '816a263e7954a5ceb4cc608f61a89640'
-  const method = 'http'
+ // const serial = '816a263e7954a5ceb4cc608f61a89640'
+  //const method = 'http'
+  const method = 'azure-small'
 
-  return ( 
-    <Slideshow serial={serial} camera={1} method={method}/> 
-  )
+  const html = nice.map( (serial, idx) => {
+    return (
+      <div key={idx}>
+        <Slideshow serial={serial} camera={1} method={method}/> 
+        <Slideshow serial={serial} camera={2} method={method}/>
+      </div>
+    )
+  })
+  
+  return html
+
 }
 
 export default Index;
