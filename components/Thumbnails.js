4dcdca4809cd2a5850eb36ae1c9762b12eb2ceb5
jdimm <92388963+jdimm@users.noreply.github.com>
2022-05-28 07:37:17 -0700

    key added.

--- components/Thumbnails.js
+++ components/Thumbnails.js
@@ -5,8 +5,8 @@ import styles from './Slideshow.module.css'
 const Thumbnails = ( {} ) => {
    const html = thumbnails.map ( (item, idx) => {
 
-       const h = <a title={item.title} href={item.url}><img src={"/screenshots/" + item.image} /></a>
-       return <div className={styles.thumbnails}>
+       const h = <a title={item.title} href={item.url}><img alt={item.title} src={"/screenshots/" + item.image} /></a>
+       return <div key={idx} className={styles.thumbnails}>
            {h}
            {item.title}
        </div>
