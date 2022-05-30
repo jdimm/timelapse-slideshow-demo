650b68183ac06c027d8d47be948c1347d480b8f1
jdimm <92388963+jdimm@users.noreply.github.com>
2022-05-30 11:30:33 -0700

    Viewport tweak.

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ const MyApp = ({ Component, pageProps }) =>{
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=7.0, minimum-scale=0.5" />
       </Head>
       <Component {...pageProps} />
     </>
