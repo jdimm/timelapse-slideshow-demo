119a096ab2e9103a5826c2ca9a799e59a3dfe249
jdimm <92388963+jdimm@users.noreply.github.com>
2022-05-30 11:27:44 -0700

    Set viewport to allow full page view on mobile.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 // pages/_app.js
 import '../styles/globals.css'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }) =>{
   return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86" />
+      </Head>
       <Component {...pageProps} />
+    </>
   )
 }
 
