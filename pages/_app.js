3963bc3383e31e9e32b2377dccb9ed83cf624555
jdimm <92388963+jdimm@users.noreply.github.com>
2022-06-01 10:39:39 -0700

    minimum-scale changed from 0.5 to 0.2.

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ const MyApp = ({ Component, pageProps }) =>{
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=7.0, minimum-scale=0.5" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=7.0, minimum-scale=0.2" />
       </Head>
       <Component {...pageProps} />
     </>
